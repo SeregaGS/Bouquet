b810af945bbc28fef6679363e879866c12a12790
SeregaGS <serega.gaskarov@rambler.ru>
2026-04-14 16:44:08 +0500

    Add FlowersModels and update MainPresenter to manage product data and loading states

--- src/main.js
+++ src/main.js
@@ -7,42 +7,50 @@ import HeaderCartPresenter from "./presenter/header-cart-presenter";
 import HeroPresenter from "./presenter/hero-presenter";
 import MainPresenter from "./presenter/main-presenter";
 
+import FlowersApiServices from "./api-services/flowers-api";
+import FlowersModels from "./model/products-model";
+
+const AUTHORIZATION = 'Basic hS2sfS44wcW2Sa2w';
+const END_POINT = 'https://grading.objects.htmlacademy.pro/flowers-shop/';
+
 const body = document.querySelector("body");
 const headerContainerCount = body.querySelector(".header__container");
 const mainContainer = body.querySelector("main");
 
+const flowersProducts = new FlowersModels(new FlowersApiServices(END_POINT, AUTHORIZATION));
+
 const headerCartPresenter = new HeaderCartPresenter(headerContainerCount);
 const heroPresenter = new HeroPresenter(mainContainer);
-const mainPresenter = new MainPresenter(mainContainer);
+const mainPresenter = new MainPresenter(mainContainer, flowersProducts);
 
 headerCartPresenter.init();
 heroPresenter.init();
 mainPresenter.init();
-
+flowersProducts.init();
 // Код для работы попапов, не удаляйте его
-window.addEventListener("DOMContentLoaded", () => {
-  iosVhFix();
-
-  window.addEventListener("load", () => {
-    // Инициализация слайдера
-    const imageSlider = new ImageSlider(".image-slider");
-    imageSlider.init();
-
-    // Инициализация попапов
-    initModals();
-  });
-
-  // Пример кода для открытия попапа
-  document
-    .querySelector(".element-which-is-open-popup")
-    .addEventListener("click", () => modals.open("popup-data-attr"));
-
-  // Код отработает, если разметка попапа уже отрисована в index.html
-
-  // Если вы хотите рисовать разметку попапа под каждое "открытие",
-  // то не забудьте перенесети в код addEventListener инициализацию слайдера
-
-  // ------------
-
-  // Ваш код...
-});
+// window.addEventListener("DOMContentLoaded", () => {
+//   iosVhFix();
+//
+//   window.addEventListener("load", () => {
+//     // Инициализация слайдера
+//     const imageSlider = new ImageSlider(".image-slider");
+//     imageSlider.init();
+//
+//     // Инициализация попапов
+//     initModals();
+//   });
+//
+//   // Пример кода для открытия попапа
+//   document
+//     .querySelector(".element-which-is-open-popup")
+//     .addEventListener("click", () => modals.open("popup-data-attr"));
+//
+//   // Код отработает, если разметка попапа уже отрисована в index.html
+//
+//   // Если вы хотите рисовать разметку попапа под каждое "открытие",
+//   // то не забудьте перенесети в код addEventListener инициализацию слайдера
+//
+//   // ------------
+//
+//   // Ваш код...
+// });
--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -2,26 +2,36 @@ import AdvantagesView from '../views/advantages-view';
 import MissionView from '../views/mission-view';
 import FilterReasonView from '../views/filter-reason-view';
 import FilterColorContainer from '../views/filter-color-container-view';
+import LoadingView from '../views/loading-view';
+import LoadingErrorView from '../views/error-loading-view';
+
+import CataloguePresenter from "../presenter/catalog-presenter";
 import {render, remove} from '../framework/render';
 
 export default class MainPresenter {
+  #loadingComponent = new LoadingView();
+  #loadingErrorComponent = new LoadingErrorView();
+
   #advantagesView = null;
   #missionView = null;
   #filterReasonView = null;
   #filterColorContainer = null;
 
-  #container = null;
+  #cataloguePresenter = null;
+  #isLoading= true;
 
-  constructor(container) {
+  #container = null;
+  #products = null;
+
+  constructor(container, products) {
     this.#container = container;
+    this.#products = products;
+
+    this.#products.addObserver(this.#onData);
   }
   init = () => {
-    this.#renderMissionComponent();
-    this.#renderAdvantagesComponent();
-    this.#renderFilterReasonComponent();
-    this.#renderFilterColorContainerComponent()
+    this.#renderCatalog();
   }
-
   #renderFilterColorContainerComponent() {
     this.#filterColorContainer = new FilterColorContainer();
     render(this.#filterColorContainer, this.#container);
@@ -30,14 +40,36 @@ export default class MainPresenter {
     this.#filterReasonView = new FilterReasonView();
     render(this.#filterReasonView, this.#container);
   }
-
   #renderMissionComponent() {
     this.#missionView = new MissionView();
     render(this.#missionView, this.#container);
   }
-
   #renderAdvantagesComponent() {
     this.#advantagesView = new AdvantagesView();
     render(this.#advantagesView, this.#container);
+  }
+
+  #onData = () => {
+    this.#isLoading = false;
+    this.#renderCatalog();
+  }
+  #renderCatalog = () => {
+    if(this.#isLoading) {
+      return render(this.#loadingComponent, this.#container);
+    }
+    remove(this.#loadingComponent)
+
+    if (this.#products.get().length === 0) {
+      return render(this.#loadingErrorComponent, this.#container);
+    }
+    remove(this.#loadingErrorComponent);
+
+    // this.#renderMissionComponent();
+    // this.#renderAdvantagesComponent();
+    this.#renderFilterReasonComponent();
+    this.#renderFilterColorContainerComponent()
+
+    this.#cataloguePresenter = new CataloguePresenter(this.#container, this.#products);
+    this.#cataloguePresenter.init()
   }
 }
