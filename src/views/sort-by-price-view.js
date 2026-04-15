6ad22f18e83b92a325fa904dae8896ba340cbabf
SeregaGS <serega.gaskarov@rambler.ru>
2026-04-16 00:23:42 +0500

    Enhance SortByPriceView to support active sorting state and button click handling

--- src/views/sort-by-price-view.js
+++ src/views/sort-by-price-view.js
@@ -1,16 +1,32 @@
 import AbstractView from '../framework/view/abstract-view';
+import { SortType } from '../const';
 
-const createSortByPriceTemplate = () =>
+const createSortByPriceTemplate = (activeSort) =>
   `
     <div class="catalogue__sorting">
       <div class="sorting-price">
         <h3 class="title sorting-price__title">Цена</h3>
-        <a class="sorting-price__link sorting-price__link--incr sorting-price__link--active" href="#" aria-label="сортировка по возрастанию цены">
+        <a
+          class="
+            sorting-price__link
+            sorting-price__link--incr
+            ${(activeSort === SortType.INCREMENT) ? 'sorting-price__link--active' : ''}"
+          href="#"
+          data-sort-by-price="${SortType.INCREMENT}"
+          aria-label="сортировка по возрастанию цены"
+          >
           <svg class="sorting-price__icon" width="50" height="46" aria-hidden="true">
             <use xlink:href="#icon-increase-sort"></use>
           </svg>
         </a>
-        <a class="sorting-price__link" href="#" aria-label="сортировка по убыванию цены">
+        <a
+          class="
+            sorting-price__link
+            ${(activeSort === SortType.DECREMENT) ? 'sorting-price__link--active' : ''}"
+          href="#"
+          data-sort-by-price="${SortType.DECREMENT}"
+          aria-label="сортировка по убыванию цены"
+          >
           <svg class="sorting-price__icon" width="50" height="46" aria-hidden="true">
             <use xlink:href="#icon-descending-sort"></use>
           </svg>
@@ -19,7 +35,33 @@ const createSortByPriceTemplate = () =>
     </div>
   `
 export default class SortByPriceView extends AbstractView {
+  #currentSort = null
+
+  constructor(currentSort) {
+    super();
+    this.#currentSort = currentSort;
+  }
   get template() {
-    return createSortByPriceTemplate();
+    return createSortByPriceTemplate(this.#currentSort);
+  }
+
+  buttonClickHandler(callback) {
+    this._callback.buttonClick = callback;
+    this.element.addEventListener('click', this.#buttonClickHandler);
+  }
+
+  #buttonClickHandler = (evt) => {
+    evt.preventDefault();
+    const link = evt.target.closest('.sorting-price__link');
+
+    if(link) {
+      this._callback.buttonClick(link.dataset.sortByPrice);
+    }
+    // if(evt.target.tagName === null || evt.target.closest('.sorting-price__link')) {
+    //   const link = evt.target.closest('.sorting-price__link');
+    //   this._callback.buttonClick(link.dataset.sortByPrice);
+    // }
+    // const link = evt.target.closest('.sorting-price__link');
+    // this._callback.buttonClick(link.dataset.sortByPrice);
   }
 }
