33c7bea700f016df83ef2e1be58cc06918f664a9
SeregaGS <serega.gaskarov@rambler.ru>
2026-04-16 15:21:37 +0500

    Add flower count constant and product type labels to const.js

--- src/const.js
+++ src/const.js
@@ -2,3 +2,12 @@ export const SortType = {
   INCREMENT: 'increment',
   DECREMENT: 'decrement',
 }
+export const COUNT_FLOWERS = 6;
+export const TypeLabelProducts = {
+  birthdayboy: 'Имениннику',
+  forlove: 'Любимой',
+  colleagues: 'Коллеге',
+  motherday: 'Маме',
+  bridge: 'Невесте',
+  default: 'Для всех'
+}
