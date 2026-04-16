ddb464b9833bb24ed255c39ebc162f91e9f76aa0
SeregaGS <serega.gaskarov@rambler.ru>
2026-04-16 23:36:41 +0500

    Add getProductId method to FlowersApiServices for fetching individual product details

--- src/api-services/flowers-api.js
+++ src/api-services/flowers-api.js
@@ -3,4 +3,6 @@ import ApiService from "../framework/api-service";
 export default class FlowersApiServices extends ApiService {
   get = () => this._load({url: 'products'})
     .then(ApiService.parseResponse);
+  getProductId = (productId) => this._load({url: `products/${productId}`})
+    .then(ApiService.parseResponse);
 }
