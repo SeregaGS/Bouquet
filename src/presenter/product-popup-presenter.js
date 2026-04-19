3cdf70460bc5b284bc5eb9226d54b50dcaf3add4
SeregaGS <serega.gaskarov@rambler.ru>
2026-04-19 21:07:42 +0500

    Add close button functionality to ProductPopupPresenter and handle escape key events

--- src/presenter/product-popup-presenter.js
+++ src/presenter/product-popup-presenter.js
@@ -7,8 +7,11 @@ export default class ProductPopupPresenter {
 
   #popupComponent = null;
 
-  constructor(container) {
+  #closeBtnClickHandler = null;
+
+  constructor(container, closeBtnClickHandler) {
     this.#container = container;
+    this.#closeBtnClickHandler = closeBtnClickHandler;
   }
 
   init = (flower) => {
@@ -17,17 +20,41 @@ export default class ProductPopupPresenter {
     const prevFlowerCardComponent = this.#popupComponent;
 
     this.#popupComponent = new ProductItemPopupView(this.#product);
+    this.#popupComponent.setCloseButtonClickHandler(this.#handleClose);
 
-    if(prevFlowerCardComponent === null) {
-      return render(this.#popupComponent, this.#container);
+    document.addEventListener('keydown', this.#onEscKeyDown);
+    document.addEventListener('click', this.#onOverlayClick);
+
+    if (prevFlowerCardComponent === null) {
+      render(this.#popupComponent, this.#container);
+    } else {
+      replace(this.#popupComponent, prevFlowerCardComponent);
+      remove(prevFlowerCardComponent);
     }
 
-    replace(this.#popupComponent, prevFlowerCardComponent);
-    remove(prevFlowerCardComponent);
   }
 
   destroy() {
+    if(this.#popupComponent === null) {
+      return;
+    }
     remove(this.#popupComponent);
+    this.#popupComponent = null;
+    document.removeEventListener('keydown', this.#onEscKeyDown);
+    document.removeEventListener('click', this.#onOverlayClick);
   }
-
+  #onEscKeyDown = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      this.#handleClose();
+    }
+  };
+  #onOverlayClick = (evt) => {
+    if (evt.target.closest('.modal__overlay')) {
+      this.#handleClose()
+    }
+  }
+  #handleClose = () => {
+    this.#closeBtnClickHandler();
+  }
 }
