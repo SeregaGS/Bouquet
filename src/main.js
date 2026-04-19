7a6a4ca8f1ab9267f9584c060966befe70b5fe24
SeregaGS <serega.gaskarov@rambler.ru>
2026-04-19 21:07:13 +0500

    Refactor main.js to streamline modal initialization and remove commented-out code

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import "./vendor";
-import { ImageSlider } from "./utils/image-slider";
 import { iosVhFix } from "./utils/ios-vh-fix";
-import { modals, initModals } from "./modals/init-modals";
+import { initModals } from "./modals/init-modals";
 
 import HeaderCartPresenter from "./presenter/header-cart-presenter";
 import HeroPresenter from "./presenter/hero-presenter";
@@ -13,44 +12,26 @@ import FlowersModels from "./model/products-model";
 const AUTHORIZATION = 'Basic hS2sfS44wcW2Sa2w';
 const END_POINT = 'https://grading.objects.htmlacademy.pro/flowers-shop/';
 
-const body = document.querySelector("body");
-const headerContainerCount = body.querySelector(".header__container");
-const mainContainer = body.querySelector("main");
 
-const flowersProducts = new FlowersModels(new FlowersApiServices(END_POINT, AUTHORIZATION));
+window.addEventListener("DOMContentLoaded", () => {
+  iosVhFix();
 
-const headerCartPresenter = new HeaderCartPresenter(headerContainerCount);
-const heroPresenter = new HeroPresenter(mainContainer);
-const mainPresenter = new MainPresenter(mainContainer, flowersProducts);
+  window.addEventListener("load", () => {
+    initModals();
+  });
 
-headerCartPresenter.init();
-heroPresenter.init();
-mainPresenter.init();
-flowersProducts.init();
-// Код для работы попапов, не удаляйте его
-// window.addEventListener("DOMContentLoaded", () => {
-//   iosVhFix();
-//
-//   window.addEventListener("load", () => {
-//     // Инициализация слайдера
-//     const imageSlider = new ImageSlider(".image-slider");
-//     imageSlider.init();
-//
-//     // Инициализация попапов
-//     initModals();
-//   });
-//
-//   // Пример кода для открытия попапа
-//   document
-//     .querySelector(".element-which-is-open-popup")
-//     .addEventListener("click", () => modals.open("popup-data-attr"));
-//
-//   // Код отработает, если разметка попапа уже отрисована в index.html
-//
-//   // Если вы хотите рисовать разметку попапа под каждое "открытие",
-//   // то не забудьте перенесети в код addEventListener инициализацию слайдера
-//
-//   // ------------
-//
-//   // Ваш код...
-// });
+  const body = document.querySelector("body");
+  const headerContainerCount = body.querySelector(".header__container");
+  const mainContainer = body.querySelector("main");
+
+  const flowersProducts = new FlowersModels(new FlowersApiServices(END_POINT, AUTHORIZATION));
+
+  const headerCartPresenter = new HeaderCartPresenter(headerContainerCount);
+  const heroPresenter = new HeroPresenter(mainContainer);
+  const mainPresenter = new MainPresenter(mainContainer, flowersProducts);
+
+  headerCartPresenter.init();
+  heroPresenter.init();
+  mainPresenter.init();
+  flowersProducts.init();
+});
