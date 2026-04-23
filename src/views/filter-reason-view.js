220f797848de035df3862cefb790e8f4422f92e5
SeregaGS <serega.gaskarov@rambler.ru>
2026-04-23 21:19:50 +0500

    Refactor filter reason view to dynamically generate filter options and improve template structure

--- src/views/filter-reason-view.js
+++ src/views/filter-reason-view.js
@@ -1,44 +1,66 @@
-import AbstractStatefulView from '../framework/view/abstract-stateful-view';
+import AbstractView from '../framework/view/abstract-view';
+import { TypeLabelProducts } from '../const'
 
-const createFilterReasonTemplate = () =>
-  `
-  <section class="filter-reason">
-    <div class="container">
-      <h2 class="title title--h3 filter-reason__title">Выберите повод для букета</h2>
-      <form class="filter-reason__form" action="#" method="post">
-        <div class="filter-reason__form-fields">
-          <div class="filter-field-text filter-reason__form-field--for-all filter-reason__form-field">
-            <input class="filter-field-text__input filter-reason__form-field--for-all filter-reason__form-field" type="radio" id="filter-reason-field-id-0" name="reason" value="for-all" checked>
-            <label class="filter-field-text__label" for="filter-reason-field-id-0"><span class="filter-field-text__text">Для всех</span></label>
+const createFilterReasonItem = ({key, name}, index, currentFilters) => {
+  return `
+      <div class="filter-field-text filter-reason__form-field--for-${key} filter-reason__form-field">
+        <input
+          class="filter-field-text__input filter-reason__form-field--for-${key} filter-reason__form-field"
+          type="radio"
+          id="filter-reason-field-id-${key}"
+          name="reason"
+          value="for-${key}"
+          ${key === currentFilters ? 'checked' : ''}
+          >
+        <label
+          class="filter-field-text__label"
+          for="filter-reason-field-id-${index}"
+          >
+          <span class="filter-field-text__text" data-filter-reason="${key}">${TypeLabelProducts[name]}</span>
+        </label>
+       </div>`
+}
+const createFilterReasonTemplate = (filters, currentFilters) => {
+ const filterItems = filters.map((filter, index) =>
+   createFilterReasonItem(filter, index, currentFilters))
+   .join('');
+
+  return `
+     <section class="filter-reason">
+       <div class="container">
+        <h2 class="title title--h3 filter-reason__title">Выберите повод для букета</h2>
+        <form class="filter-reason__form" action="#" method="post">
+          <div class="filter-reason__form-fields">
+            ${filterItems}
           </div>
-          <div class="filter-field-text filter-reason__form-field--for-birthday filter-reason__form-field">
-            <input class="filter-field-text__input filter-reason__form-field--for-birthday filter-reason__form-field" type="radio" id="filter-reason-field-id-1" name="reason" value="for-birthday">
-            <label class="filter-field-text__label" for="filter-reason-field-id-1"><span class="filter-field-text__text">Имениннику</span></label>
-          </div>
-          <div class="filter-field-text filter-reason__form-field--for-bride filter-reason__form-field">
-            <input class="filter-field-text__input filter-reason__form-field--for-bride filter-reason__form-field" type="radio" id="filter-reason-field-id-2" name="reason" value="for-bride">
-            <label class="filter-field-text__label" for="filter-reason-field-id-2"><span class="filter-field-text__text">Невесте</span></label>
-          </div>
-          <div class="filter-field-text filter-reason__form-field--for-mother filter-reason__form-field">
-            <input class="filter-field-text__input filter-reason__form-field--for-mother filter-reason__form-field" type="radio" id="filter-reason-field-id-3" name="reason" value="for-mother">
-            <label class="filter-field-text__label" for="filter-reason-field-id-3"><span class="filter-field-text__text">Маме</span></label>
-          </div>
-          <div class="filter-field-text filter-reason__form-field--for-colleague filter-reason__form-field">
-            <input class="filter-field-text__input filter-reason__form-field--for-colleague filter-reason__form-field" type="radio" id="filter-reason-field-id-4" name="reason" value="for-colleague">
-            <label class="filter-field-text__label" for="filter-reason-field-id-4"><span class="filter-field-text__text">Коллеге</span></label>
-          </div>
-          <div class="filter-field-text filter-reason__form-field--for-darling filter-reason__form-field">
-            <input class="filter-field-text__input filter-reason__form-field--for-darling filter-reason__form-field" type="radio" id="filter-reason-field-id-5" name="reason" value="for-darling">
-            <label class="filter-field-text__label" for="filter-reason-field-id-5"><span class="filter-field-text__text">Любимой</span></label>
-          </div>
-        </div>
-        <button class="filter-reason__btn visually-hidden" type="submit" tabindex="-1">применить фильтр</button>
-      </form>
-    </div>
-  </section>
-  `
-export default class FilterReasonView extends AbstractStatefulView {
+        </form>
+       </div>
+     </section>
+  `;
+}
+export default class FilterReasonView extends AbstractView {
+  #filters = null;
+  #currentFilters = null;
+
+  constructor(filters, currentFilters) {
+    super();
+    this.#filters = filters;
+    this.#currentFilters = currentFilters;
+  }
   get template() {
-    return createFilterReasonTemplate();
+    return createFilterReasonTemplate(this.#filters, this.#currentFilters);
+  }
+
+  setFilterTypeClickHandler(callback) {
+    this._callback.filterType = callback;
+    this.element.addEventListener('click', this.#filterTypeHandler);
+  }
+
+  #filterTypeHandler = (evt) => {
+    evt.preventDefault();
+    if(evt.target.dataset.filterReason === undefined) {
+      return;
+    }
+    this._callback.filterType(evt.target.dataset.filterReason);
   }
 }
