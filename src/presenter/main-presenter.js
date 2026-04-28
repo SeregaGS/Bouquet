3d83d1fbbfba8a5070ae6ff7fb5f9e34c8ca54c8
SeregaGS <serega.gaskarov@rambler.ru>
2026-04-28 22:38:30 +0500

    Refactor MainPresenter to improve product handling and integrate popup rendering

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -5,65 +5,92 @@ import LoadingErrorView from '../views/error-loading-view';
 import FilterReasonPresenter from '../presenter/filter-reason-presenter'
 import FilterColorPresenter from '../presenter/filter-color-presenter'
 import CataloguePresenter from "../presenter/catalog-presenter";
+import ProductPopupPresenter from './product-popup-presenter';
 import {render, remove} from '../framework/render';
+import { modals } from "../modals/init-modals";
 
 export default class MainPresenter {
   #loadingComponent = new LoadingView();
   #loadingErrorComponent = new LoadingErrorView();
+  #missionView = new MissionView();
+  #advantagesView = new AdvantagesView();
 
-  #advantagesView = null;
-  #missionView = null;
   #filterReasonPresenter = null;
   #filterColorPresenter = null;
-
   #cataloguePresenter = null;
+  #productPopupPresenter = null;
+
   #isLoading= true;
+  #selectedProduct = null;
 
   #container = null;
-  #products = null;
-  #filterType = null;
 
-  constructor(container, products, filterReason) {
+  #productsModel = null;
+  #filterModel = null;
+
+  constructor(container, productsModel, filterModel) {
     this.#container = container;
-    this.#products = products;
-    this.#filterType = filterReason;
+    this.#productsModel = productsModel;
+    this.#filterModel = filterModel;
 
-    this.#products.addObserver(this.#onData);
+    this.#productsModel.addObserver(this.#onData);
   }
   init = () => {
+    this.#renderAdvantagesMission();
     this.#renderCatalog();
-  }
-  #renderMissionComponent() {
-    this.#missionView = new MissionView();
-    render(this.#missionView, this.#container);
-  }
-  #renderAdvantagesComponent() {
-    this.#advantagesView = new AdvantagesView();
-    render(this.#advantagesView, this.#container);
   }
 
   #onData = () => {
     this.#isLoading = false;
     this.#renderCatalog();
   }
+  #renderAdvantagesMission = () => {
+    render(this.#missionView, this.#container);
+    render(this.#advantagesView, this.#container);
+  }
   #renderCatalog = () => {
     if(this.#isLoading) {
-      return render(this.#loadingComponent, this.#container);
+      render(this.#loadingComponent, this.#container);
+      return;
     }
     remove(this.#loadingComponent);
-
-    if (this.#products.get().length === 0) {
-      return render(this.#loadingErrorComponent, this.#container);
+    if (this.#productsModel.get().length === 0) {
+      render(this.#loadingErrorComponent, this.#container);
+      return;
     }
     remove(this.#loadingErrorComponent);
-    // this.#renderMissionComponent();
-    // this.#renderAdvantagesComponent();
-    this.#filterReasonPresenter = new FilterReasonPresenter(this.#container, this.#filterType);
-    this.#filterReasonPresenter.init();
-    this.#filterColorPresenter = new FilterColorPresenter(this.#container, this.#filterType);
-    this.#filterColorPresenter.init();
-    this.#cataloguePresenter = new CataloguePresenter(this.#container, this.#products, this.#filterType);
-    this.#cataloguePresenter.init();
 
+    this.#filterReasonPresenter = new FilterReasonPresenter(this.#container, this.#filterModel);
+    this.#filterReasonPresenter.init();
+    this.#filterColorPresenter = new FilterColorPresenter(this.#container, this.#filterModel);
+    this.#filterColorPresenter.init();
+    this.#cataloguePresenter = new CataloguePresenter(this.#container, this.#productsModel, this.#filterModel, this.#clickOpenPopup);
+    this.#cataloguePresenter.init();
+  }
+
+  #renderPopup = (id) => {
+    const contentContainer = document.querySelector('.modal-product');
+    // const imageSlider = new ImageSlider(".image-slider");
+
+    if(this.#productPopupPresenter) {
+      this.#removeProductPopup();
+    }
+    this.#productPopupPresenter = new ProductPopupPresenter(contentContainer, this.#removeProductPopup)
+    this.#productPopupPresenter.init(id);
+    modals.open('popup-data-attr');
+    // imageSlider.init();
+  }
+  #removeProductPopup = () => {
+    if (!this.#productPopupPresenter) {
+      return;
+    }
+    this.#productPopupPresenter.destroy();
+    this.#productPopupPresenter = null;
+    this.#selectedProduct = null;
+    modals.close('popup-data-attr');
+  }
+  #clickOpenPopup = async (id) => {
+    this.#selectedProduct = await this.#productsModel.loadProductDetails(id);
+    this.#renderPopup(this.#selectedProduct);
   }
 }
