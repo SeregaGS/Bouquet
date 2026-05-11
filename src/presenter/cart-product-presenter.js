1302751918234ee0b7e50fab99836340c91498b4
SeregaGS <serega.gaskarov@rambler.ru>
2026-05-11 21:44:51 +0500

    Add CartProductPresenter for managing cart product interactions

---
+++ src/presenter/cart-product-presenter.js
@@ -0,0 +1,46 @@
+import CartDeferredProductItemView from '../views/cart-deferred-item-view';
+import { render, replace, remove } from '../framework/render';
+
+export default class CartProductPresenter {
+  #container = null;
+  #cartModel = null;
+
+  #decrement = null
+  #increment = null;
+  #delete = null;
+
+  #productComponent = null;
+
+  constructor(container, cartModel, decrement, increment, del) {
+    this.#container = container;
+    this.#cartModel = cartModel;
+    this.#decrement = decrement;
+    this.#increment = increment;
+    this.#delete = del;
+  }
+
+  init(flower) {
+    const obj = this.#cartModel.get().products;
+    const result = obj[flower.id];
+
+    const prevFlowerCardComponent = this.#productComponent;
+    this.#productComponent = new CartDeferredProductItemView(flower, result);
+    this.#productComponent.setDecrementCartProduct((id) => this.#decrement(id));
+    this.#productComponent.setIncrementCartProduct((id) => this.#increment(id));
+    this.#productComponent.setClearCartProduct((id) => this.#delete(id));
+
+    if(prevFlowerCardComponent === null) {
+      render(this.#productComponent, this.#container);
+      return;
+    }
+
+    replace(this.#productComponent, prevFlowerCardComponent);
+    remove(prevFlowerCardComponent);
+  }
+
+  destroy() {
+    remove(this.#productComponent);
+    this.#productComponent = null;
+  }
+
+}
