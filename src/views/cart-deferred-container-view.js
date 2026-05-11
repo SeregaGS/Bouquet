0082466a9cf2977f4048959da3f3684a55db94f4
SeregaGS <serega.gaskarov@rambler.ru>
2026-05-11 21:41:22 +0500

    Add CartDeferredContainerView class with template for popup deferred cart container

---
+++ src/views/cart-deferred-container-view.js
@@ -0,0 +1,10 @@
+import AbstractView from '../framework/view/abstract-view';
+
+const createCartDeferredContainerTemplate = () =>
+  `<div class="popup-deferred__container"></div>`;
+
+export default class CartDeferredContainerView extends AbstractView {
+  get template() {
+    return createCartDeferredContainerTemplate();
+  }
+}
