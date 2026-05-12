192f0289e64951f80235be7de5c58557c031f6be
SeregaGS <serega.gaskarov@rambler.ru>
2026-05-12 23:34:58 +0500

    Fix formatting by simplifying the catalog product list template in catalog-product-list-view.js

--- src/views/catalog-product-list-view.js
+++ src/views/catalog-product-list-view.js
@@ -1,11 +1,7 @@
 import AbstractView from '../framework/view/abstract-view';
 
-const createCatalogProductListTemplate = () =>
-  `
-   <ul class="catalogue__list">
+const createCatalogProductListTemplate = () => `<ul class="catalogue__list"></ul>`;
 
-   </ul>
-  `
 export default class CatalogProductListView extends AbstractView {
   get template() {
     return createCatalogProductListTemplate();
