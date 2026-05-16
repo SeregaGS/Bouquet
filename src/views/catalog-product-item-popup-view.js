133f5a778ee55bdb0bfcb607d74e6c7939372659
SeregaGS <serega.gaskarov@rambler.ru>
2026-05-16 13:14:42 +0500

    Refactor ProductItemPopupView to enhance button state management and improve user feedback

--- src/views/catalog-product-item-popup-view.js
+++ src/views/catalog-product-item-popup-view.js
@@ -2,7 +2,14 @@ import AbstractStatefulView from '../framework/view/abstract-stateful-view';
 import { createImageSliderPopup } from './catalog-product-item-popup-images-view'
 
 const createProductItemPopupTemplate = (flower) => {
-  const {title, price, description, isAdding } = flower
+  const {title, price, description, isAdding, isDisabled } = flower;
+
+  let buttonText = !isAdding ? 'отложить' : 'отложено';
+
+  if(isDisabled) {
+    buttonText = !isAdding ? 'откладываем...' : 'возвращаем...';
+  }
+
   return `<div>
            <div class="image-slider swiper modal-product__slider">
               <div class="image-slides-list swiper-wrapper">
@@ -25,8 +32,9 @@ const createProductItemPopupTemplate = (flower) => {
               <b class="price price--size-big">${price}<span>Р</span></b>
             </div>
             <p class="text text--size-40">${description}</p>
-            <button class="btn btn--outlined btn--full-width product-description__button" type="button" data-focus>
-              ${!isAdding ? 'отложить' : 'отложено'}
+            <button class="btn btn--outlined btn--full-width product-description__button" type="button" data-focus
+            ${isDisabled ? 'disabled' : ''}>
+              ${buttonText}
             </button>
           </div>
         </div>
@@ -35,15 +43,12 @@ const createProductItemPopupTemplate = (flower) => {
 export default class ProductItemPopupView extends AbstractStatefulView {
   constructor(flower, isAdding) {
     super();
-    this._state = {...flower, isAdding};
+    this._state = {...flower, isAdding, isDisabled: false};
   }
   get template() {
     return createProductItemPopupTemplate(this._state);
   }
 
-  setCloseButtonHandler = (callback)=> {
-    this._callback.closePopup = callback;
-  }
   setAddToCartButtonHandler = (callback)=> {
     this._callback.addToCart = callback;
     this.element
@@ -53,11 +58,10 @@ export default class ProductItemPopupView extends AbstractStatefulView {
 
   #addToCartButtonHandler = (evt) => {
     evt.preventDefault();
-    this._callback.addToCart(evt.target);
+    this._callback.addToCart();
   }
 
   _restoreHandlers = () => {
-    this.setCloseButtonHandler(this._callback.closePopup);
     this.setAddToCartButtonHandler(this._callback.addToCart);
   }
 
